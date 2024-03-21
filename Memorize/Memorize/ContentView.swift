1fd558c349b8c07102cd4ec10a94fd1e1b8f7d81
허승빈 <gjtmdqls159@naver.com>
2024-03-21 10:13:55 +0900

    first message

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by 허승빈 on 3/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, CS193p")
+            Text("집에 가고싶다")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 허승빈 on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
